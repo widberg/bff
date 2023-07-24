2c7f33fbd547b8fad7d7cb1339800821e0a6223b
pizzart <46490176+pizzart@users.noreply.github.com>
2023-07-24 14:50:19 +0300

    add lightdata

--- bff/src/class/mod.rs
+++ bff/src/class/mod.rs
@@ -6,6 +6,7 @@ use self::bitmap::Bitmap;
 use self::collision_vol::CollisionVol;
 use self::game_obj::GameObj;
 use self::light::Light;
+use self::light_data::LightData;
 use self::lod::Lod;
 use self::material::Material;
 use self::mesh::Mesh;
@@ -30,6 +31,7 @@ pub mod bitmap;
 pub mod collision_vol;
 pub mod game_obj;
 pub mod light;
+pub mod light_data;
 pub mod lod;
 pub mod material;
 pub mod mesh;
@@ -82,6 +84,7 @@ classes! {
     CollisionVol,
     GameObj,
     Light,
+    LightData,
     Lod,
     Material,
     Mesh,
