b989f6c028b694ff3d708eb2872e8a0901c6ba35
widberg <jaredwidberg@gmail.com>
2023-09-27 20:03:15 -0400

    Add new test for reading fuel object formats

--- bff/tests/bigfile.rs
+++ bff/tests/bigfile.rs
@@ -4,7 +4,10 @@ mod tests {
     use std::path::PathBuf;
 
     use bff::bigfile::BigFile;
+    use bff::class::Class;
     use bff::platforms::Platform;
+    use bff::traits::TryFromVersionPlatform;
+    use bff::versions::Version;
     use binrw::io::BufReader;
     use test_generator::test_resources;
 
@@ -20,5 +23,25 @@ mod tests {
         let f = File::open(bigfile_path).unwrap();
         let mut reader = BufReader::new(f);
         let _ = BigFile::read_platform(&mut reader, platform).unwrap();
+    }
+
+    #[test_resources("data/bigfiles/FUEL/PC_US/v1_381_67_09/**/*.DPC")]
+    fn read_objects_fuel(bigfile_path_str: &str) {
+        let mut bigfile_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        bigfile_path.pop();
+        bigfile_path.push(bigfile_path_str);
+        let platform = match bigfile_path.extension() {
+            Some(extension) => extension.try_into().unwrap_or(Platform::PC),
+            None => Platform::PC,
+        };
+        let f = File::open(bigfile_path).unwrap();
+        let mut reader = BufReader::new(f);
+        let bigfile = BigFile::read_platform(&mut reader, platform).unwrap();
+
+        for object in bigfile.objects().values() {
+            assert!(object.body().len() > 0);
+            let _ = Class::try_from_version_platform(object, Version::V1_381_67_09, Platform::PC)
+                .unwrap();
+        }
     }
 }
