b108e5b978dc6b63e93f6f40e83c1ae6e940cb01
widberg <jaredwidberg@gmail.com>
2023-10-17 15:04:14 -0400

    Make LZ4 work with All.psc

--- bff/src/lz.rs
+++ bff/src/lz.rs
@@ -2,11 +2,11 @@ use std::cmp::{max, min};
 use std::io::{Read, Seek, SeekFrom, Write};
 use std::ptr::null_mut;
 
-use binrw::{BinRead, BinReaderExt, BinResult, BinWrite, BinWriterExt};
-use lz4::{Decoder, EncoderBuilder};
+use binrw::{args, BinRead, BinReaderExt, BinResult, BinWrite, BinWriterExt};
+use binrw::Endian;
 use minilzo_rs::LZO;
 
-use crate::{BffResult, Endian};
+use crate::{BffResult};
 
 #[binrw::parser(reader, endian)]
 pub fn decompress_body_parser(decompressed_size: u32, compressed_size: u32) -> BinResult<Vec<u8>> {
@@ -417,19 +417,31 @@ pub fn lzo_decompress<R: Read>(reader: &mut R, _endian: Endian) -> BffResult<Vec
     Ok(decompressed)
 }
 
-#[binrw::writer(writer, endian)]
+#[binrw::writer(writer)]
+pub fn lz4_compress_data_writer(data: &[u8]) -> BinResult<()> {
+    // TODO: Don't use asserts. Add proper error handling.
+    // TODO: Not the same parameters used by the game.
+    let compress_bound = unsafe { lz4::liblz4::LZ4_compressBound(data.len() as i32) as usize};
+    let mut compressed_buffer: Vec<u8> = Vec::with_capacity(compress_bound);
+    unsafe {
+        let result = lz4::liblz4::LZ4_compress_default(data.as_ptr() as *const i8, compressed_buffer.as_mut_ptr() as *mut i8, data.len() as i32, compressed_buffer.capacity() as i32);
+        assert!(result >= 0);
+        compressed_buffer.set_len(result as usize);
+    }
+    writer.write_all(&compressed_buffer)?;
+    Ok(())
+}
+
+#[binrw::writer(writer)]
 pub fn lz4_compress_data_with_header_writer_internal(data: &[u8]) -> BinResult<()> {
     let begin = writer.stream_position()?;
     writer.seek(SeekFrom::Current(8))?;
-    let mut encoder = EncoderBuilder::new().build(writer)?;
-    encoder.write_all(data)?;
-    let (writer, result) = encoder.finish();
-    result?;
+    lz4_compress_data_writer(data, writer, Endian::Little, ())?;
     let end = writer.stream_position()?;
 
     writer.seek(SeekFrom::Start(begin))?;
-    (data.len() as u32).write_options(writer, endian, ())?;
-    ((end - begin - 8) as u32).write_options(writer, endian, ())?;
+    (data.len() as u32).write_le(writer)?;
+    ((end - begin - 8) as u32).write_le(writer)?;
     writer.seek(SeekFrom::Start(end))?;
     Ok(())
 }
@@ -447,16 +459,26 @@ pub fn lz4_compress_data_with_header_writer<W: Write + Seek>(
     )?)
 }
 
-#[binrw::parser(reader, endian)]
+#[binrw::parser(reader)]
+pub fn lz4_decompress_data_parser(decompressed_size: u32, compressed_size: u32) -> BinResult<Vec<u8>> {
+    // TODO: Don't use asserts. Add proper error handling.
+    let compressed_buffer = Vec::<u8>::read_args(reader, args! { count: compressed_size as usize })?;
+    let mut decompressed_buffer: Vec<u8> = Vec::with_capacity(decompressed_size as usize);
+    unsafe {
+        let result = lz4::liblz4::LZ4_decompress_safe(compressed_buffer.as_ptr() as *const i8, decompressed_buffer.as_mut_ptr() as *mut i8, compressed_size as i32, decompressed_size as i32);
+        assert!(result >= 0);
+        decompressed_buffer.set_len(result as usize);
+    }
+    assert_eq!(decompressed_buffer.len(), decompressed_size as usize);
+
+    Ok(decompressed_buffer)
+}
+
+#[binrw::parser(reader)]
 pub fn lz4_decompress_data_with_header_parser_internal() -> BinResult<Vec<u8>> {
-    let _decompressed_size = u32::read_options(reader, endian, ())?;
-    let _compressed_size = u32::read_options(reader, endian, ())?;
-    let mut decoder = Decoder::new(reader)?;
-    let mut decompressed: Vec<u8> = Vec::new();
-    decoder.read_to_end(&mut decompressed)?;
-    let (_reader, result) = decoder.finish();
-    result?;
-    Ok(decompressed)
+    let decompressed_size = u32::read_le(reader)?;
+    let compressed_size = u32::read_le(reader)?;
+    lz4_decompress_data_parser(reader, Endian::Little, (decompressed_size, compressed_size))
 }
 
 pub fn lz4_decompress_data_with_header_parser<R: Read + Seek>(
