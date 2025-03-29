563a1a5ef5eb85faea693597243446b742c91398
widberg <jaredwidberg@gmail.com>
2025-03-28 22:52:53 -0400

    [CLI] Use name db when naming extracted objects
    Co-authored-by: SabeMP <sabemasterprox@gmail.com>

--- bff-cli/src/extract.rs
+++ bff-cli/src/extract.rs
@@ -51,6 +51,23 @@ pub fn read_bigfile(bigfile_path: &Path) -> BffCliResult<BigFile> {
     Ok(BigFile::read_platform(&mut reader, platform)?)
 }
 
+const INVALID_PATH_CHARS: [u8; 41] = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+    26, 27, 28, 29, 30, 31, 34, 42, 47, 58, 60, 62, 63, 92, 124,
+];
+
+fn clean_path(path: String) -> String {
+    path.chars()
+        .map(|v| {
+            if INVALID_PATH_CHARS.contains(&(v as u8)) {
+                '_'
+            } else {
+                v
+            }
+        })
+        .collect()
+}
+
 pub fn extract(
     bigfile_path: &Path,
     directory: &Path,
@@ -71,9 +88,17 @@ pub fn extract(
     std::fs::create_dir(&resources_path)?;
 
     for resource in bigfile.objects.values() {
-        let name = resource.name;
+        let name = clean_path(format!("{}", resource.name));
         let class_name = resource.class_name;
-        let path = resources_path.join(format!("{}.{}", name, class_name));
+        let mut path = resources_path.join(format!("{}.{}", name, class_name));
+        let mut i = 0;
+        // TODO: Should check if the file contains the same object and overwrite it.
+        // This will be easier once there is a bff header for the files.
+        // Also need to think about a "--sync" option to apply names after an extract.
+        while path.exists() {
+            path.set_file_name(format!("{}_{}.{}", name, i, class_name));
+            i += 1;
+        }
         let mut writer = BufWriter::new(File::create(path)?);
         bigfile.dump_resource(resource, &mut writer)?;
     }
