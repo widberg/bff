1cf24a0d3af994a96aeac03f3731380d14121763
widberg <jaredwidberg@gmail.com>
2025-04-13 17:48:41 -0400

    [Core] Add MQFEL classes
    Co-authored-by: Vi <82741743+SamuraiOndo@users.noreply.github.com>

--- bff/src/class/mod.rs
+++ bff/src/class/mod.rs
@@ -9,8 +9,10 @@ use self::camera::Camera;
 use self::camera_zone::CameraZone;
 use self::collision_vol::CollisionVol;
 use self::collision_vol_data::CollisionVolData;
+use self::conductor::Conductor;
 use self::decal::Decal;
 use self::dialog_event::DialogEvent;
+use self::entity::Entity;
 use self::flare::Flare;
 use self::flare_data::FlareData;
 use self::fog_volume::FogVolume;
@@ -41,6 +43,7 @@ use self::node::Node;
 use self::occluder::Occluder;
 use self::omni::Omni;
 use self::omni_data::OmniData;
+use self::r#override::Override;
 use self::particles::Particles;
 use self::particles_data::ParticlesData;
 use self::prefab::Prefab;
@@ -49,22 +52,27 @@ use self::reflection_probe::ReflectionProbe;
 use self::rot_shape::RotShape;
 use self::rot_shape_data::RotShapeData;
 use self::rtc::Rtc;
+use self::shader::Shader;
 use self::skel::Skel;
 use self::skin::Skin;
+use self::skindata::SkinData;
 use self::sound::Sound;
 use self::sound_event::SoundEvent;
+use self::specialeffectnode::SpecialEffectNode;
 use self::spline::Spline;
 use self::spline_graph::SplineGraph;
 use self::spline_zone::SplineZone;
 use self::surface::Surface;
 use self::surface_datas::SurfaceDatas;
 use self::terrain::Terrain;
+use self::texture::Texture;
 use self::txt::Txt;
 use self::user_define::UserDefine;
 use self::user_define_script::UserDefineScript;
 use self::warp::Warp;
 use self::world::World;
 use self::world_ref::WorldRef;
+use self::xrefnode::XRefNode;
 use crate::macros::classes::classes;
 
 pub mod anim_frame;
@@ -78,8 +86,10 @@ pub mod camera;
 pub mod camera_zone;
 pub mod collision_vol;
 pub mod collision_vol_data;
+pub mod conductor;
 pub mod decal;
 pub mod dialog_event;
+pub mod entity;
 pub mod flare;
 pub mod flare_data;
 pub mod fog_volume;
@@ -110,6 +120,7 @@ pub mod node;
 pub mod occluder;
 pub mod omni;
 pub mod omni_data;
+pub mod r#override;
 pub mod particles;
 pub mod particles_data;
 pub mod prefab;
@@ -118,16 +129,20 @@ pub mod reflection_probe;
 pub mod rot_shape;
 pub mod rot_shape_data;
 pub mod rtc;
+pub mod shader;
 pub mod skel;
 pub mod skin;
+pub mod skindata;
 pub mod sound;
 pub mod sound_event;
+pub mod specialeffectnode;
 pub mod spline;
 pub mod spline_graph;
 pub mod spline_zone;
 pub mod surface;
 pub mod surface_datas;
 pub mod terrain;
+pub mod texture;
 pub mod trivial_class;
 pub mod txt;
 pub mod user_define;
@@ -135,6 +150,7 @@ pub mod user_define_script;
 pub mod warp;
 pub mod world;
 pub mod world_ref;
+pub mod xrefnode;
 
 classes! {
     Animation,
@@ -148,8 +164,10 @@ classes! {
     CameraZone,
     CollisionVol,
     CollisionVolData,
+    Conductor,
     Decal,
     DialogEvent,
+    Entity,
     Flare,
     FlareData,
     FogVolume,
@@ -180,6 +198,7 @@ classes! {
     Occluder,
     Omni,
     OmniData,
+    Override,
     Particles,
     ParticlesData,
     Prefab,
@@ -188,20 +207,25 @@ classes! {
     RotShape,
     RotShapeData,
     Rtc,
+    Shader,
     Skel,
     Skin,
+    SkinData,
     Sound,
     SoundEvent,
+    SpecialEffectNode,
     Spline,
     SplineGraph,
     SplineZone,
     Surface,
     SurfaceDatas,
     Terrain,
+    Texture,
     Txt,
     UserDefine,
     UserDefineScript,
     Warp,
     World,
     WorldRef,
+    XRefNode,
 }
